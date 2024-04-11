1d65d22741ef86bc3de9200b9e6d4cdc5eeb012d
MariamShindy <125688137+MariamShindy@users.noreply.github.com>
2024-04-11 14:42:05 +0200

    Update Database

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -6,7 +6,7 @@ namespace Talabat.APIs
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
 			var webApplicationBuilder = WebApplication.CreateBuilder(args);
 
@@ -27,6 +27,20 @@ namespace Talabat.APIs
 			#endregion
 
 			var app = webApplicationBuilder.Build();
+			//Ask CLR for creating object from DBContext explicitly
+			using var scope = app.Services.CreateScope();
+			var services = scope.ServiceProvider;
+			var _dbContext = services.GetRequiredService<StoreContext>();
+			var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+			try
+			{
+				await _dbContext.Database.MigrateAsync(); 
+			}
+			catch (Exception ex)
+			{
+				var logger = loggerFactory.CreateLogger<Program>();
+				logger.LogError(ex , "An error has been occured during applying the migration");
+			}
 
 			#region Configure kestrel middleware
 			// Configure the HTTP request pipeline.
