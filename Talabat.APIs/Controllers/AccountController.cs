75dd9e07f8e8fa36c427d091e1b9d2ce29957e37
MariamShindy <125688137+MariamShindy@users.noreply.github.com>
2024-04-28 15:38:20 +0300

    Login Endpoint

---
+++ Talabat.APIs/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.APIs.Dtos;
+using Talabat.APIs.Errors;
+using Talabat.Core.Entities.Identity;
+
+namespace Talabat.APIs.Controllers
+{
+
+	public class AccountController : BaseApiController
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly SignInManager<ApplicationUser> _signInManager;
+
+		public AccountController(UserManager<ApplicationUser> userManager ,SignInManager<ApplicationUser> signInManager)
+        {
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+		[HttpPost("login")]
+		public async Task<ActionResult<UserDto>> Login(LoginDto model)
+		{
+			var user = await _userManager.FindByEmailAsync(model.Email);
+			if (user == null)
+			{
+				return Unauthorized(new ApiResponse(401 , "Invalid login"));
+			}
+			var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password,false);
+			if(!result.Succeeded)
+				return Unauthorized(new ApiResponse(401, "Invalid login"));
+			return Ok(new UserDto()
+			{
+				Email = user.Email,
+				DisplayName = user.DisplayName,
+				Token = "this is token"
+			});
+		}
+	}
+}
