8f3efe2084befb4ec5abdf5a2106821c910ccd7e
MariamShindy <125688137+MariamShindy@users.noreply.github.com>
2024-05-06 01:30:24 +0300

    Order Module - Configuration Classes

---
+++ Talabat.Infrastructure/_Data/Config/OrderConfig/OrderConfigurations.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.OrderAggregate;
+
+namespace Talabat.Infrastructure._Data.Config.OrderConfig
+{
+	internal class OrderConfigurations : IEntityTypeConfiguration<Order>
+	{
+		public void Configure(EntityTypeBuilder<Order> builder)
+		{
+			builder.OwnsOne(order => order.ShippingAddress, ShippingAddress => ShippingAddress.WithOwner());
+			builder.Property(order => order.Status).HasConversion(
+				(OStatus) => OStatus.ToString() ,
+				(OStatus) => (OrderStatus) Enum.Parse(typeof(OrderStatus),OStatus)
+				) ;
+			//builder.HasOne(order => order.DeliveryMethod).WithMany();
+			builder.Property(order => order.Subtotal).HasColumnType("decimal(12,2)");
+		}
+	}
+}
---
+++ Talabat.Infrastructure/_Data/Config/OrderConfig/OrderItemConfigurations.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.OrderAggregate;
+
+namespace Talabat.Infrastructure._Data.Config.OrderConfig
+{
+	internal class OrderItemConfigurations : IEntityTypeConfiguration<OrderItem>
+	{
+		public void Configure(EntityTypeBuilder<OrderItem> builder)
+		{
+			builder.OwnsOne(orderItem => orderItem.Product, Product => Product.WithOwner());
+			builder.Property(orderItem => orderItem.Price).HasColumnType("decimal(12,2)");
+		}
+	}
+}
