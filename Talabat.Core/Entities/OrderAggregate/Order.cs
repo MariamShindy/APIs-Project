4ffd73e43b52787556dad6a5525d18264a241bce
MariamShindy <125688137+MariamShindy@users.noreply.github.com>
2024-05-06 00:54:46 +0300

    Order Module - Entities

---
+++ Talabat.Core/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities.OrderAggregate
+{
+	public class Order : BaseEntity
+	{
+		public string BuyerEmail { get; set; } = null!;
+		public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.UtcNow;
+		public OrderStatus Status { get; set; } = OrderStatus.Pending;
+		public Address ShippingAddress { get; set; } = null!;
+        public int DeliveryMethodId { get; set; } //Foreign key 
+        public  DeliveryMethod DeliveryMethod { get; set; } //Navigational property one
+		public ICollection<OrderItem> Items { get; set;} = new HashSet<OrderItem>();
+        public decimal Subtotal { get; set; } //Total cost - shipping fees
+        public String PaymentIntentId { get; set; } = string.Empty;
+
+        //[NotMapped]
+        //public decimal Total => DeliveryMethod.Cost + Subtotal; 
+
+        public decimal GetTotal() => DeliveryMethod.Cost + Subtotal;
+	}
+}
+ 
