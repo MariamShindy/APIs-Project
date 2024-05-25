545ab1c3931789c9f59a964450d98bad823ad1ea
MariamShindy <125688137+MariamShindy@users.noreply.github.com>
2024-05-25 22:56:10 +0300

    CreateOrUpdatePaymentIntent EndPoint

---
+++ Talabat.Core/Services.Contract/IPaymentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.Basket;
+
+namespace Talabat.Core.Services.Contract
+{
+	public interface IPaymentService
+	{
+		Task<CustomerBasket?> CreateOrUpdatePaymentIntent(string basketId);
+	}
+}
